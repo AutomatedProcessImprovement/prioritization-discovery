96fbbdb0a968492422a2cb1717adb7351d9685dc
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2022-10-12 15:46:29 +0300

    Refactor prioritization discovery

--- src/prioritization-discovery/discovery.py
+++ src/prioritization-discovery/discovery.py
@@ -13,30 +13,24 @@ def discover_prioritized_instances(event_log: pd.DataFrame, log_ids: EventLogIDs
     :return: a copy of [event_log] with a column identifying the activities involved in prioritization.
     """
     event_log = event_log.copy()
-
+    prioritizations = []
+    # Analyze grouped by resource
     for resource, events in event_log.groupby(log_ids.resource):
         # Sort them by enabled time
         events.sort_values([log_ids.enabled_time, log_ids.start_time], inplace=True)
-        # Collect events that were delayed due to others being prioritized
-        delayed_events = []
+        # For each event that could have others prioritized
         previous_event = None
-        for event in events.iterrows():
-            # If current event was prioritized w.r.t. previous event
-            if (
-                    previous_event is not None and
-                    previous_event[log_ids.enabled_time] < event[log_ids.enabled_time] and
-                    previous_event[log_ids.start_time] > event[log_ids.start_time]
-            ):
-                # Save event to analyze
-                delayed_events += [previous_event]
+        for index, event in events.iterrows():
+            # If following events are enabled after current start
+            if previous_event is not None and previous_event[log_ids.start_time] > event[log_ids.enabled_time]:
+                # Get events prioritized w.r.t. the previous one
+                prioritized_events = event_log[
+                    (event_log[log_ids.enabled_time] > previous_event[log_ids.enabled_time]) &
+                    (event_log[log_ids.start_time] < previous_event[log_ids.start_time])
+                    ]
+                # Save info about the prioritized group
+                print("{} prioritized over {}".format([e[log_ids.activity] for e in prioritized_events], previous_event[log_ids.activity]))
+                prioritizations += [(previous_event, prioritized_events)]
             # Jump to next event
             previous_event = event
-        # For each delayed event, make a group with the ones prioritized to it
-        for delayed_event in delayed_events:
-            # Get events prioritized w.r.t. the delayed one
-            prioritized_events = event_log[
-                (event_log[log_ids.enabled_time] > delayed_event[log_ids.enabled_time]) &
-                (event_log[log_ids.start_time] < delayed_event[log_ids.start_time])
-                ]
-            # TODO Save info about the prioritized group
-    return event_log
+    return prioritizations
